784e22b1bc52e09275674e849dd5553d335a2f11
Shivansh <shivanshawasthi33@gmail.com>
2024-08-25 17:13:02 +0530

    edits

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import express from 'express'
 const app = express();
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
 
 dotenv.config()
-const port = process.env.PORT
+
+
+
+const port = process.env.PORT;
 
 
 
 
-    (async () => {
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log("connected")
+    } catch (error) {
+        console.error(error);
+        throw error
 
-    })()
+    }
+})()
 
 
 
