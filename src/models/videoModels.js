cf132caa98435823a573705c4d3312d827d84a6e
Shivansh <shivanshawasthi33@gmail.com>
2024-08-27 03:19:14 +0530

    added agregate pipeline

--- src/models/videoModels.js
+++ src/models/videoModels.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 
 const videoSchema = new mongoose.Schema({
@@ -36,6 +37,8 @@ const videoSchema = new mongoose.Schema({
     }]
 }, { timestamps: true })
 
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 
 const Video = mongoose.model("Video", videoSchema);
